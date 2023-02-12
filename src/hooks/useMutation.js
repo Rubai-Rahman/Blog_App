df798462794951e83463ac96eb6267d95c0f2402
RubaiRahman <rubairahman1@gmail.com>
2023-02-12 23:05:46 +0600

    create post added

--- src/hooks/useMutation.js
+++ src/hooks/useMutation.js
@@ -1,9 +1,11 @@
 import { gql } from "@apollo/client";
 
 export const INSERT_BLOG_POST = gql`
-  mutation InsertBlogPost($title: String!, $content: String!) {
-    insert_blog_post_one(object: { title: $title, content: $content }) {
+  mutation InsertBlogPost($title: String!, $content: String! ,$category:String!,$author_id:String!) {
+    insert_blog_posts_one(object: { title: $title, content: $content,category:$category,author_id:$author_id }) {
       id
+      is_published
+      date
     }
   }
 `;
--- src/pages/blogs/CreatePost.js
+++ src/pages/blogs/CreatePost.js
@@ -1,9 +1,56 @@
+import { useMutation } from "@apollo/client";
+import { useUserData } from "@nhost/react";
 import React from "react";
+import { INSERT_BLOG_POST } from "../../hooks/useMutation";
 
 const CreatePost = () => {
+  let titleInput, contentInput, categoryInput, author_idInput;
+  const user = useUserData();
+
+  author_idInput = user.displayName;
+  console.log(titleInput, contentInput, categoryInput, author_idInput);
+  const [insertPost] = useMutation(INSERT_BLOG_POST);
+  
   return (
     <div>
-      <h2>Create A Post </h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          insertPost({
+            variables: {
+              title: titleInput.value,
+              content: contentInput.value,
+              category: categoryInput.value,
+              author_id: author_idInput,
+            },
+          });
+          titleInput.value = "";
+          contentInput.value = "";
+        }}
+      >
+        <h2>Create a new post</h2>
+
+        <input
+          type="text"
+          placeholder="Enter your title"
+          ref={(i) => (titleInput = i)}
+        ></input>
+        <select name="" id="" ref={(i) => (categoryInput = i)}>
+          <option value="News">News</option>
+          <option value="Technology">Technology</option>
+          <option value="News">Medicine</option>
+          <option value="News">Programming</option>
+          <option value="News">Environment</option>
+          <option value="News">Cars</option>
+        </select>
+
+        <textarea
+          placeholder="Write your post here..."
+          ref={(i) => (contentInput = i)}
+        ></textarea>
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
